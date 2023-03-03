f470c44fdfed2496f785c7d5d94a1ac0ef3ae322
VardanVardanyanCSUS <vardanvardanyan@csus.edu>
2023-03-02 16:47:34 -0800

    first commit Assignment 5

---
+++ fancifymytext.js
@@ -0,0 +1,31 @@
+function alertPrompt(){
+    //alert("Hello, World!");
+    let textArea = document.getElementById("textArea");
+    textArea.style.fontSize = "24px";
+
+
+}
+//think i need to use an event listerner
+function radioButton(){
+    let fancySchmancy = document.getElementById("fancySchmancy");
+    let boringBetty = document.getElementById("boringBetty");
+    if(fancySchmancy.checked){
+        //boringBetty.checked = false;
+        textArea.style.fontWeight = "bold";
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
+        //boringBetty.checked = false;
+    }
+     if (boringBetty.checked){
+        //fancySchmancy.checked = false;
+        document.getElementById("textArea").style.fontWeight = "normal";
+        //fancySchmancy.checked = false;
+        
+    }
+    //document.getElementById("textArea").style.fontWeight = "bold";
+}
+
+function Moo(){
+    textArea.
+}
+
